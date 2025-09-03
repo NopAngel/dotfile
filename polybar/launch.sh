2fc8677b0a8a0c3f4997c81788e596e55f862a20
NoAngel <155915830+NopAngel@users.noreply.github.com>
2025-09-03 18:22:08 -0400

    my new's config: pretty desktop!

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,14 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "Bars launched..."
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar main --config=~/.config/polybar/config.ini &
+polybar second --config=~/.config/polybar/config.ini &
+
+echo "Polybar launched..."
